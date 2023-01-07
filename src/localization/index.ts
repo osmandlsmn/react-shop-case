d6f8b812e391bd01a62815a07602daa13fa12bdd
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2023-01-08 01:29:46 +0300

    feat: add i18n

---
+++ src/localization/index.ts
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import en from "./en/translations.json";
+import tr from "./tr/translations.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    tr: { translation: tr },
+  },
+  lng: "tr",
+  fallbackLng: "en",
+  debug: true,
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import Router from "@/router";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import "@/localization/index";
 import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
